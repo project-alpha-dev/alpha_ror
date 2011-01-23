65c81ab4c49828864892e740adc334f8cfd85a90
Alpha Dev <project.alpha.dev@gmail.com>
2011-01-23 17:02:18 +1100

    fendy: initial commit for alpha project under RoR 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphaRor::Application.config.secret_token = 'd354d9999f2cb1a35bed2abbcd83803666b56e14e30e99c4c0fb6c16b8abf6e615e5046fcdb0e5963312ac803d43c3450bedc9f28ab6dc13e0148dd82584419e'
